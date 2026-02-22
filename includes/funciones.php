ea054469abb41d7ff1e23c44110b258ebdb82c67
Alejandro Curiel <jcuriel@tiprotec.com>
2026-02-21 19:23:52 -0600

    Add authentication check function

--- includes/funciones.php
+++ includes/funciones.php
@@ -5,3 +5,10 @@ function incluirTemplate(string $nombre, bool $inicio = false)
 {
   include TEMPLATES_URL . "{$nombre}.php";
 }
+
+function estaAutenticado(): bool
+{
+  session_start();
+  $auth = $_SESSION['login'];
+  return $auth ? true : false;
+}
