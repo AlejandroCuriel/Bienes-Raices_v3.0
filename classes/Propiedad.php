938916a6cf39ec481f34bfb5a2b4057b39e8b076
Alejandro Curiel <alex_curielg@hotmail.com>
2026-03-26 16:46:18 -0600

    feat: enhance Propiedad class

--- classes/Propiedad.php
+++ classes/Propiedad.php
@@ -5,4 +5,31 @@ namespace App;
 class Propiedad extends ActiveRecord
 {
   protected static $tabla = 'propiedades';
+
+  protected static $columnasDB = ['id', 'titulo', 'precio', 'imagen', 'descripcion', 'habitaciones', 'wc', 'estacionamiento', 'vendedorId', 'creado'];
+
+  public $id;
+  public $titulo;
+  public $imagen;
+  public $descripcion;
+  public $precio;
+  public $habitaciones;
+  public $wc;
+  public $estacionamiento;
+  public $vendedorId;
+  public $creado;
+
+  public function __construct($args = [])
+  {
+    $this->id = $args['id'] ?? null;
+    $this->titulo = $args['titulo'] ?? '';
+    $this->imagen = $args['imagen'] ?? '';
+    $this->descripcion = $args['descripcion'] ?? '';
+    $this->precio = $args['precio'] ?? '';
+    $this->habitaciones = $args['habitaciones'] ?? '';
+    $this->wc = $args['wc'] ?? '';
+    $this->estacionamiento = $args['estacionamiento'] ?? '';
+    $this->vendedorId = $args['vendedorId'] ?? '';
+    $this->creado = date('Y/m/d');
+  }
 }
