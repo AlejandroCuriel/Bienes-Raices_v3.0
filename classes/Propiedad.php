2ae73812c538b427c8dfe4364734c33127fdf6f2
Alejandro Curiel <alex_curielg@hotmail.com>
2026-03-27 10:40:32 -0600

    feat: add validation method to Propiedad class

--- classes/Propiedad.php
+++ classes/Propiedad.php
@@ -32,4 +32,34 @@ class Propiedad extends ActiveRecord
     $this->vendedorId = $args['vendedorId'] ?? '';
     $this->creado = date('Y/m/d');
   }
+
+  public function validar()
+  {
+    if (!$this->titulo) {
+      self::$errores[] = "Debes añadir un título";
+    }
+    if (!$this->precio) {
+      self::$errores[] = "El precio es obligatorio";
+    }
+    if (strlen($this->descripcion) < 50) {
+      self::$errores[] = "La descripción debe tener al menos 50 caracteres";
+    }
+    if (!$this->habitaciones) {
+      self::$errores[] = "El número de habitaciones es obligatorio";
+    }
+    if (!$this->wc) {
+      self::$errores[] = "El número de baños es obligatorio";
+    }
+    if (!$this->estacionamiento) {
+      self::$errores[] = "El número de estacionamientos es obligatorio";
+    }
+    if (!$this->vendedorId) {
+      self::$errores[] = "Elige un vendedor";
+    }
+    if (!$this->imagen) {
+      self::$errores[] = "La imagen es obligatoria";
+    }
+
+    return self::$errores;
+  }
 }
