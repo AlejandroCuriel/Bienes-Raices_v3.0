ee7d2395856076d7d3359e6125d01b94dfb870c9
Alejandro Curiel <alex_curielg@hotmail.com>
2026-03-27 15:18:29 -0600

    feat: create form for add and update vendedores

---
+++ includes/templates/formulario_vendedores.php
@@ -0,0 +1,20 @@
+ <!-- Formulario para crear o actualizar vendedores -->
+ <fieldset>
+   <legend>Información General</legend>
+
+   <label for="nombre">Nombre:</label>
+   <input type="text" id="nombre" name="vendedor[nombre]" placeholder="Nombre vendedor" value="<?php echo sanitizarHTML($vendedor->nombre); ?>" pattern="[a-zA-Z]+">
+
+   <label for="apellido">Apellido:</label>
+   <input type="text" id="apellido" name="vendedor[apellido]" placeholder="Apellido vendedor" value="<?php echo sanitizarHTML($vendedor->apellido); ?>" pattern="[a-zA-Z]+">
+
+   <label for="telefono">Telefono:</label>
+   <input type="tel" id="telefono" name="vendedor[telefono]" maxlength="10" placeholder="Telefono vendedor" value="<?php echo sanitizarHTML($vendedor->telefono); ?>" pattern="[0-9]{10}">
+
+   <label for="imagen">Imagen de Perfil:</label>
+   <small>La imagen debe ser en formato JPG o PNG (Máximo 150kb)</small>
+   <input type="file" id="imagen" accept="image/jpeg, image/png" name="vendedor[imagen]">
+   <?php if ($vendedor->imagen) : ?>
+     <img class="imagen-small" src="../src/img/<?php echo $vendedor->imagen; ?>" alt="Foto de perfil de <?= $vendedor->nombre ?>">
+   <?php endif; ?>
+ </fieldset>
