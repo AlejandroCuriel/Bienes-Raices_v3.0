60f4eb88dc00dfaec87dd5e58320485c9b1f74c3
Alejandro Curiel <alex_curielg@hotmail.com>
2026-03-27 13:23:53 -0600

    feat: remove redundant id property from Vendedor

--- classes/Vendedor.php
+++ classes/Vendedor.php
@@ -7,7 +7,6 @@ class Vendedor extends ActiveRecord
   protected static $tabla = 'vendedores';
   protected static $columnasDB = ['id', 'nombre', 'apellido', 'telefono'];
 
-  public $id;
   public $nombre;
   public $apellido;
   public $telefono;
