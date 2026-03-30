5671f5d3f58aec5a4e078046fa7a2c383920f2a2
Alejandro Curiel <alex_curielg@hotmail.com>
2026-03-30 10:12:19 -0600

    feat: reorganize vendedor  and update image

--- includes/templates/formulario_vendedores.php
+++ includes/templates/formulario_vendedores.php
@@ -8,13 +8,18 @@
    <label for="apellido">Apellido:</label>
    <input type="text" id="apellido" name="vendedor[apellido]" placeholder="Apellido vendedor" value="<?php echo sanitizarHTML($vendedor->apellido); ?>" pattern="[a-zA-Z]+">
 
-   <label for="telefono">Telefono:</label>
-   <input type="tel" id="telefono" name="vendedor[telefono]" maxlength="10" placeholder="Telefono vendedor" value="<?php echo sanitizarHTML($vendedor->telefono); ?>" pattern="[0-9]{10}">
-
    <label for="imagen">Imagen de Perfil:</label>
    <small>La imagen debe ser en formato JPG o PNG (Máximo 150kb)</small>
    <input type="file" id="imagen" accept="image/jpeg, image/png" name="vendedor[imagen]">
    <?php if ($vendedor->imagen) : ?>
-     <img class="imagen-small" src="../src/img/<?php echo $vendedor->imagen; ?>" alt="Foto de perfil de <?= $vendedor->nombre ?>">
+     <img class="imagen-perfil" src="/imagenes_perfil/<?php echo $vendedor->imagen; ?>" alt="Foto de perfil de <?= $vendedor->nombre ?>" width="80" height="80">
    <?php endif; ?>
+ </fieldset>
+
+ <fieldset>
+   <legend>Información Extra</legend>
+
+   <label for="telefono">Telefono:</label>
+   <input type="tel" id="telefono" name="vendedor[telefono]" maxlength="10" placeholder="Telefono vendedor" value="<?php echo sanitizarHTML($vendedor->telefono); ?>" pattern="[0-9]{10}">
+
  </fieldset>
