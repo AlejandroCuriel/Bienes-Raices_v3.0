71a2c45d182dbcbd2ac281a408271df12bc8e1ee
Alejandro Curiel <alex_curielg@hotmail.com>
2026-03-30 10:15:21 -0600

    feat: create update functionality for vendedor

---
+++ admin/vendedores/actualizar.php
@@ -0,0 +1,82 @@
+<?php
+
+use App\Vendedor;
+use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\ImageManager as Image;
+
+require_once '../../includes/app.php';
+
+estaAutenticado();
+
+// Validar que sea un ID válido
+$id = $_GET['id'];
+$id = filter_var($id, FILTER_VALIDATE_INT);
+
+if (!$id) {
+  header('Location: /admin');
+}
+
+// Obtener los datos del vendedor
+$vendedor = Vendedor::find($id);
+
+// Consulta para obtener los vendedores
+$vendedores = Vendedor::all();
+
+// Array para los errores
+$errores = Vendedor::getErrores();
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+  // Asignar los atributos
+  $args = $_POST['vendedor'];
+
+  // Sincronizar objeto en memoria con lo que el usuario escribió
+  $vendedor->sincronizar($args);
+
+  // Validaciones
+  $errores = $vendedor->validar();
+
+  // Generar nombre unico
+  $manager = new Image(new Driver());
+  $nombreImagen = md5(uniqid(rand(), true)) . '.jpg';
+
+  // Subida de archivos
+  if ($_FILES['vendedor']['tmp_name']['imagen']) {
+    // Solo para Intervention Image v3
+    $imagen = $manager->read($_FILES['vendedor']['tmp_name']['imagen'])->cover(200, 200);
+    $vendedor->setImagen($nombreImagen);
+  }
+
+  if (empty($errores)) {
+    if ($_FILES['vendedor']['tmp_name']['imagen']) {
+      // Almacenar la imagen
+      $imagen->save(CARPETA_IMAGENES_PERFIL . $nombreImagen);
+    }
+    $vendedor->guardar();
+  }
+}
+
+incluirTemplate('header');
+?>
+
+<main class="contenedor seccion">
+  <h1>Actualizar Vendedor(a)</h1>
+  <a href="/admin" class="boton boton-verde">Volver</a>
+
+  <?php foreach ($errores as $error): ?>
+    <div class="alerta error">
+      <?php print($error) ?>
+    </div>
+  <?php endforeach ?>
+
+  <form class="formulario" method="POST" enctype="multipart/form-data">
+    <?php include_once '../../includes/templates/formulario_vendedores.php' ?>
+
+    <button type="submit" class="boton boton-verde">Actualizar Vendedor</button>
+  </form>
+</main>
+
+<?php
+incluirTemplate('footer');
+?>
